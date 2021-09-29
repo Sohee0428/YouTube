f37ebbcb3002f1bf50e7cbdb717f855567b72ddd
Sohee <sohee3480@gmail.com>
2021-09-29 14:51:04 +0900

    viewBinding + 프래그먼트 연결

--- app/src/main/java/com/example/youtube/MainActivity.kt
+++ app/src/main/java/com/example/youtube/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.youtube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainer, PlayerFragment())
+            .commit()
+
     }
 }
